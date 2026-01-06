7032e01bc0933708f9750bf5a726c5cddfab1363
watcharabest <watcharabest.86@gmail.com>
2026-01-06 21:39:45 +0700

    Vercel Analysics

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { SpeedInsights } from "@vercel/speed-insights/react"
+import { Analytics } from "@vercel/analytics/react"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <Analytics />
     <SpeedInsights />
   </StrictMode>,
 )
